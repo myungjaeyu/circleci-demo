bcea38d29135bfb6cd77f53af9826d350bcdc676
u4bi <admin@naver.id>
2018-09-02 06:05:16 +0900

    tests. (#1)
    * Added app_build_test.
    
    * Deleted ssr app.

--- common/render/scripts.js
+++
@@ -1,3 +0,0 @@
-const scripts = assets => Object.keys(assets).reduce((scripts, key) => scripts + `<script src="/dev${ assets[key].js }"></script>`, '')
-
-module.exports = scripts
--- common/render/styles.js
+++
@@ -1,3 +0,0 @@
-const styles = assets => Object.keys(assets).reduce((styles, key) => styles + ( assets[key].css ? `<link rel="stylesheet" href="/dev${ assets[key].css }" />` : ''), '')
-
-module.exports = styles
